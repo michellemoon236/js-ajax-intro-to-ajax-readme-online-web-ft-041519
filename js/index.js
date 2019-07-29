64a6a239d541a5b08fc720a9e8b4c4cbb16499fd
Michelle Moon <michellemoon236@gmail.com>
2019-07-29 00:07:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
@@ -14,12 +21,7 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
  
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
-}
+
 
 function getCommits(el) {
   const name = el.dataset.repo;
